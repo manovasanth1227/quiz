f1ac1a437889edc12fbd14f4a6c5c54e8fbd9e37
Manovasanth B <manovasanth1227@gmail.com>
2020-11-27 14:06:29 +0530

    Updates

---
+++ precache-manifest.b29d2e90b6a2b807e0aea9ae33f0bca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45524555e1c7cbd9c7da1fa5cd2435b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60aef64c1d8b7c729eac",
+    "url": "/static/css/main.84c4df72.chunk.css"
+  },
+  {
+    "revision": "4266caab4a236f528a05",
+    "url": "/static/js/2.aa09e225.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.aa09e225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60aef64c1d8b7c729eac",
+    "url": "/static/js/main.a294e58d.chunk.js"
+  },
+  {
+    "revision": "10d35b5ba53f0290d324",
+    "url": "/static/js/runtime-main.cc28ea4a.js"
+  }
+]);
